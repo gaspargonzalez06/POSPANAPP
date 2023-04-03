2412a0174d28a585f1dd30e81694098ad3c9b8a2
Gaspar Gonzalez <gaspar.gonzalez3@utp.ac.ap>
2023-04-03 09:27:40 -0500

    db added

---
+++ pospan/db/pospan/detalles.php
@@ -0,0 +1,24 @@
+<?php
+require "connection.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $ordenesid = $_POST["ordenesid"];
+  
+  $query = "SELECT * FROM detalles WHERE ordenesid = '$ordenesid'";
+
+  // Ejecutar la consulta
+  $resultado = mysqli_query($conn, $query);
+
+  // Crear un array para almacenar los resultados
+  $rows = array();
+  while ($row = mysqli_fetch_assoc($resultado)) {
+    $rows[] = $row;
+  }
+
+  // Convertir el array a JSON y mostrarlo en pantalla
+  echo json_encode($rows);
+}
+
+// Cerrar la conexión
+mysqli_close($conn);
+?>
